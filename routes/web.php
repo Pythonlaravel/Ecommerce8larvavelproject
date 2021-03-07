02c195c2d74b2f961e7335431adbb44becac07ae
Shaheer <ranashaheerahmad2@gmail.com>
2021-03-07 12:18:38 -0800

    till video 18

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use App\Http\Livewire\Admin\AdminCategoryComponent;
 use App\Http\Livewire\Admin\AdminAddCategoryComponent;
 use App\Http\Livewire\Admin\AdminEditCategoryComponent;
 use App\Http\Livewire\Admin\AdminProductComponent;
+use App\Http\Livewire\Admin\AdminAddProductComponent;
+
+
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Livewire\Homecomponent;
@@ -66,4 +69,5 @@ Route::middleware(['auth:sanctum', 'verified', 'authadmin'])->group(function(){
     Route::get('/admin/category/add',AdminAddCategoryComponent::class)->name('admin.addcategory');
     Route::get('/admin/category/edit/{category_slug}',AdminEditCategoryComponent::class)->name('admin.editcategory');
     Route::get('/admin/products',AdminProductComponent::class)->name('admin.products');
+    Route::get('/admin/product/add',AdminAddProductComponent::class)->name('admin.addproduct');
 });
