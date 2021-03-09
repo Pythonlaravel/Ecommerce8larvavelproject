158c4a7fe35b1b2ef922becc2a8a19bad1d9d9ca
Shaheer <ranashaheerahmad2@gmail.com>
2021-03-09 14:19:03 -0800

    till video 21 and 34 mint

---
+++ app/Http/Livewire/Admin/AdminHomeSliderComponent.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Livewire\Admin;
+
+use Livewire\Component;
+
+class AdminHomeSliderComponent extends Component
+{
+    public function render()
+    {
+        $sliders = HomeSlider::all();
+        return view('livewire.admin.admin-home-slider-component',['sliders'=>$sliders])->layout('layouts.base');
+    }
+}
