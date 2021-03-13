9dd46b7b4ff8fdec1cdb7a3c24e33d07edd3cb69
Shaheer <ranashaheerahmad2@gmail.com>
2021-03-13 02:58:58 -0800

    till video 10

--- app/Http/Livewire/Homecomponent.php
+++ app/Http/Livewire/Homecomponent.php
@@ -10,6 +10,7 @@ class Homecomponent extends Component
     public function render()
     {
         $sliders = HomeSlider::where('status',1)->get();
-        return view('livewire.home-component',['sliders'=>$sliders])->layout("layouts.base");
+        $lproducts = Product::orderBy('created_at','DESC')->get()->take(8); 
+        return view('livewire.home-component',['sliders'=>$sliders,'lproducts=>$lproducts'])->layout("layouts.base");
     }
 }
