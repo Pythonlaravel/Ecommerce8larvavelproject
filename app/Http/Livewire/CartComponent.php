38f261e1cc16d7805f04fac8b50d8796c2027aa0
Shaheer <ranashaheerahmad2@gmail.com>
2021-03-23 12:33:06 -0700

    till video 30

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -8,13 +8,13 @@ use Cart;
 class CartComponent extends Component
 {
     public function increaseQuantity($rowId){
-        $product = Cart::get($rowId);
+        $product = Cart::instance('cart')->get($rowId);
         $qty = $product->qty + 1;
-        Cart::update($rowId,$qty);
+        Cart::instance('cart')->update($rowId,$qty);
 
     }
     public function decreaseQuantity($rowId){
-        $product = Cart::get($rowId);
+        $product = Cart::instance('cart')->get($rowId);
         $qty = $product->qty - 1;
         Cart::update($rowId,$qty);
 
@@ -22,12 +22,12 @@ class CartComponent extends Component
 
     public function destroy($rowId)
     {
-        Cart::remove($rowId);
+        Cart::instance('cart')->remove($rowId);
         session()->flash('Success_message','Item has been removed');
     }
 
     public function destroyAll(){
-        Cart::destroy();
+        Cart::instance('cart')->destroy();
     }
     public function render()
     {
--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -31,6 +31,12 @@ class ShopComponent extends Component
         session()->flash('success_message','Item added in Cart');
         return redirect()->route('product.cart');
     }
+
+    public function addToWishlist($product_id,$product_name,$product_price)
+    {
+        Cart::instance('wishlist')->add($product_id,$product_name,1,$product_price)->associate('App\Models\Product');
+    }
+
     use WithPagination;
     public function render()
     {  
